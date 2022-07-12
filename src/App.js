df20da46be5cc51cfedb4f13716778fa376536e6
Krixxeless <syrel.vladislav@gmail.com>
2022-07-12 16:13:31 +0300

    Add deployment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ let three = 0;
 let four = 0;
 let five = 0;
 
+let CharacterBanner = getCharacterBanner(characters,weapons,[16, 3, 7, 42]);
+console.log(CharacterBanner);
+
 function App() {
   return (
       <div>
@@ -14,14 +17,12 @@ function App() {
           3 rare: {three}<br/>
           4 rare: {four}<br/>
           5 rare: {five}<br/>
-          <button onClick={wish}>
-                Помолиться 1 раз
-          </button>
       </div>
   );
 }
+
 for (let i = 1; i <= 90; i++) {
-    let test = wish(getCharacterBanner(characters,weapons,[16, 3, 7, 42]));
+    let test = wish(CharacterBanner);
     console.log(test)
     if(test.rare === 3) {
         three += 1
@@ -30,7 +31,5 @@ for (let i = 1; i <= 90; i++) {
     } else  {five += 1}
 }
 
-//console.log(banner_drop_weapon(characters, weapons, [32, 38, 26, 24, 17, 53, 67]))
-//console.log(wish(getCharacterBanner(characters,weapons,[16, 3, 7, 42])))
 
 export default App;
